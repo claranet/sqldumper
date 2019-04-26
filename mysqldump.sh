c397dcf53c9166a4245f474e9a3cff29ca5c3666
Martin Weber <martin.weber@de.clara.net>
2019-04-26 15:38:07 +0200

    BugFix

--- mysqldump.sh
+++ mysqldump.sh
@@ -35,20 +35,20 @@ for C in $CONTAINERS; do
     _DUMP_USERNAME=$(docker inspect $C | jq -re '.[].Config.Labels["dumper.user"]')
     if [[ -z "${_DUMP_USERNAME}" || "${_DUMP_USERNAME}" == "null" ]]; then
         echo "Cannot find 'dumper.user' as label, try to fetch MYSQL_USER from ENV."
-        _DUMP_USERNAME=$(docker inspect $C |  jq -r '.[].Config.Env[] | select(. | contains("MYSQL_USER"))' | sed 's/MYSQL_USER=//g') | sed 's/[(^")|("$)]//g'
+        _DUMP_USERNAME=$(docker inspect $C |  jq -r '.[].Config.Env[] | select(. | contains("MYSQL_USER"))' | sed 's/MYSQL_USER=//g' | sed 's/[(^")|("$)]//g')
     fi
     test -z "${_DUMP_USERNAME}" && _DUMP_USERNAME="root"
 
     _DUMP_PASSWORD=$(docker inspect $C | jq -r '.[].Config.Labels["dumper.password"]')
     if [[ -z "${_DUMP_PASSWORD}" || "${_DUMP_PASSWORD}" == "null" ]]; then
         echo "Cannot find 'dumper.password' as label, try to use MYSQL_PASSWORD from ENV"
-        _DUMP_PASSWORD=$(docker inspect $C |  jq -r '.[].Config.Env[] | select(. | contains("MYSQL_PASSWORD"))' | sed 's/MYSQL_PASSWORD=//g') | sed 's/[(^")|("$)]//g'
+        _DUMP_PASSWORD=$(docker inspect $C |  jq -r '.[].Config.Env[] | select(. | contains("MYSQL_PASSWORD"))' | sed 's/MYSQL_PASSWORD=//g' | sed 's/[(^")|("$)]//g')
     fi
     
     _DUMP_DATABASE=$(docker inspect $C | jq -r '.[].Config.Labels["dumper.database"]')
     if [[ -z "${_DUMP_DATABASE}" || "${_DUMP_DATABASE}" == "null" ]]; then
         echo "Cannot find 'dumper.database' as label, try to use MYSQL_DATABASE from ENV"
-        _DUMP_DATABASE=$(docker inspect $C |  jq -r '.[].Config.Env[] | select(. | contains("MYSQL_DATABASE"))' | sed 's/MYSQL_DATABASE=//g') | sed 's/[(^")|("$)]//g'
+        _DUMP_DATABASE=$(docker inspect $C |  jq -r '.[].Config.Env[] | select(. | contains("MYSQL_DATABASE"))' | sed 's/MYSQL_DATABASE=//g' | sed 's/[(^")|("$)]//g')
     fi
 
     _DUMP_ARGS="-u${_DUMP_USERNAME}"
