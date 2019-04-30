214c56acd70afd3ec8a0f39c779f82e9dc34acbd
Martin Weber <martin.weber@de.clara.net>
2019-04-30 09:58:50 +0200

    Create Dumper Class and Instance for each Database Type

--- dumper.py
+++ dumper.py
@@ -23,7 +23,7 @@ With Database type "FREE" you can write your own dump command!
 __author__     = "Martin Weber <martin.weber@de.clara.net>"
 __license__    = "Apache License v2.0"
 
-__version__    = 0.0.0
+__version__    = "0.0.1"
 __maintainer__ = "Martin Weber"
 __email__      = "martin.weber@de.clara.net"
 __status__     = "Production"
@@ -32,12 +32,11 @@ import docker
 import os
 import gzip
 import fnmatch
+import glob
 import logging as log
 from datetime import datetime
 
 docker_client = docker.from_env()
-dumpdir = os.environ["DUMPDIR"]
-
 log.basicConfig(level=log.DEBUG)
 
 class ExtContainer(docker.models.containers.Container):
@@ -61,155 +60,193 @@ class ExtContainer(docker.models.containers.Container):
 
         return _env
 
-def dumpFree(container):
-    """
-    Dump any Database
+class Dumper:
+    _container = None
+    _db_type = ""
 
-    Use this Command to dump any database type. Just set dumper.command="..." to dump a database.
-    The command should dumop the Database to stdout. This Function will write it to a dump file.
-    """
+    _dump_args = []
+    _dump_env = []
 
-    if "dumper.command" in container.labels:
-        _command = container.labels["dumper.command"]
+    _database = ""
+    _username = ""
+    _password = ""
+
+    __retention = 1
+    __dumpdir = "/dumps"
+
+    def __init__(self, container):
+        self._container = container
+
+        self.__dumpdir = os.environ["DUMPDIR"]
+
+        self._db_type = self._container.labels["dumper.type"]
+
+        self.__fetchRetention()
+        self.__fetchEnv()
+        self.__fetchArgs()
+        self.__fetchCredentials()
+
+    def __fetchRetention(self):
+        self.__retention = os.environ["RETENTION"]
+        if "dumper.retention" in self._container.labels:
+            self.__retention = self._container.labels["dumper.retention"]
+
+        self.__retention = int(self.__retention)
+
+    def __fetchCredentials(self):
+        if "dumper.username" in self._container.labels:
+            self._username = self._container.labels["dumper.username"]
+        if "dumper.password" in self._container.labels:
+            self._password = self._container.labels["dumper.password"]
+        if "dumper.database" in self._container.labels:
+            self._database = self._container.labels["dumper.database"]
+
+    def __fetchEnv(self):
+        for _opt in fnmatch.filter(self._container.labels, "dumper.env.*"):
+            value = self._container.labels[_opt]
+            key = _opt.replace("dumper.env.", "")
+            self._dump_env[key] = value
     
-    log.debug("Execute Command: %s" % _command)
-    e_code, gen = container.exec_run(cmd = _command, stream = True)
+    def __fetchArgs(self):
+        for _opt in fnmatch.filter(self._container.labels, "dumper.args.*"):
+            value = self._container.labels[_opt]
+            key = _opt.replace("dumper.args.", "")
 
-    _file = "%s/%s_free_%s.sql.gz" % (dumpdir, container.name, datetime.now().strftime("%Y%m%d%I%M%S"))
-    f = gzip.open(_file, "wb")
-    for item in gen:
-        f.write(item)
-    f.close()
+            if key.len() == 1:
+                self._dump_args.append("-%s '%s'" % (key, value))
+            else:
+                self._dump_args.append("--%s='%s'" % (key, value))
 
-def dumpPostgreSQL(container):
-    """
-    Dump PostgreSQL
+    def _executeDump(self, command):
+        log.debug("Execute dump - %s" % command)
+        e_code, gen = self._container.exec_run(cmd = command, stream = True, environment=self._dump_env)
 
-    """
-    _username = None
-    _password = None
-    _database = None
+        f = gzip.open(self.filename, "wb")
+        for item in gen:
+            f.write(item)
+        f.close()
 
-    if "dumper.user" in container.labels:
-        _username = container.labels["dumper.user"]
-    elif "POSTGRES_USER" in container.environments:
-        _username = container.environments["POSTGRES_USER"]
+    def cleanup(self):
+        log.debug("Cleanup old backups - keep last %s dumps on disk" % self.__retention)
+        files = glob.glob("%s/%s__%s__*" % ( self.__dumpdir, self._container.name, self._db_type ))
+        files.sort(reverse=True)
 
-    if "dumper.password" in container.labels:
-        _password = container.labels["dumper.password"]
-    elif "POSTGRES_PASSWORD" in container.environments:
-        _password = container.environments["POSTGRES_PASSWORD"]
+        for i in range(self.__retention, len(files)):
+            log.debug("Delete dump %s" % files[i])
+            os.unlink(files[i])
+            
 
-    if "dumper.database" in container.labels:
-        _database = container.labels["dumper.database"]
-    elif "POSTGRES_DB" in container.environments:
-        _database = container.environments["POSTGRES_DB"]
+    @property
+    def filename(self):
+        _ext = "sql"
+        if self._db_type == "mongo":
+            _ext = "bak"
+        
+        _file = "%s/%s__%s__%s.%s.gz" % ( self.__dumpdir, self._container.name, self._db_type, datetime.now().strftime("%Y%m%d%I%M%S"), _ext)
+        log.debug("Dumpfile: %s" % _file)
+        return _file
 
-    _args = ""
-    if _username:
-        _args += (" --username='%s'" % _username)
-    if _database:
-        _args += (" %s" % _database)
+    def dump(self):
+        pass
 
-    _command = "pg_dump %s" % _args
-    e_code, gen = container.exec_run(cmd = _command, stream = True, environment=["PGPASSWORD=%s" % _password])
+class MySQLDumper(Dumper):
+    def dump(self):
+        log.debug("Dump MySQL")
 
-    _file = "%s/%s_postgresql_%s.sql.gz" % (dumpdir, container.name, datetime.now().strftime("%Y%m%d%I%M%S"))
-    f = gzip.open(_file, "wb")
-    for item in gen:
-        f.write(item)
-    f.close()
+        """
+        Dump MySQL/MariaDB
+        """
+        
+        if len(self._username) == 0 and "MYSQL_USER" in self._container.environments:
+            self._username = self._container.environments["MYSQL_USER"]
 
+        if len(self._password) == 0 and "MYSQL_PASSWORD" in self._container.environments:
+            self._password = self._container.environments["MYSQL_PASSWORD"]
 
-def dumpMongoDB(container):
-    """
-    Dump MongoDB
+        if len(self._database) == 0 and "MYSQL_DATABASE" in self._container.environments:
+            self._database = self._container.environments["MYSQL_DATABASE"]
 
-    user, password and database have to be pass. This Using MongoDB, this values would not fetch by Environments.
-    Dump Parameter can be extend using dumper.mongo.<option>. For example "dumper.mongo.authenticationDatabase=admin"
-    will add "--authenticationDatabase=admin" as parameter to mongodump.
+        _args = " ".join(self._dump_args)
+        if self._username:
+            _args += (" --u '%s'" % self._username)
+        if self._password:
+            _args += (" -p'%s'" % self._password)
+        if self._database:
+            _args += (" %s" % self._database)
 
-    Note that authenticationDatabase=admin will be set as default if it is not passed
-    """
+        _command = "mysqldump %s" % _args
+        self._executeDump(_command)
 
-    _username = None
-    _password = None
-    _database = None
+class PostgresDumper(Dumper):
+    def dump(self):
+        """
+        Dump PostgreSQL
 
-    if "dumper.user" in container.labels:
-        _username = container.labels["dumper.user"]
+        """
+        if len(self._username) == 0 and "POSTGRES_USER" in self._container.environments:
+            self._username = self._container.environments["POSTGRES_USER"]
 
-    if "dumper.password" in container.labels:
-        _password = container.labels["dumper.password"]
+        if len(self._password) == 0 and "POSTGRES_PASSWORD" in self._container.environments:
+            self._password = self._container.environments["POSTGRES_PASSWORD"]
 
-    if "dumper.database" in container.labels:
-        _database = container.labels["dumper.database"]
+        if len(self._database) == 0 and "POSTGRES_DB" in self._container.environments:
+            self._database = self._container.environments["POSTGRES_DB"]
 
-    _args = ["--quiet", "--archive"]
-    for _opt in fnmatch.filter(container.labels, "dumper.mongo.*"):
-        value = container.labels[_opt]
-        key = _opt.replace("dumper.mongo.", "")
-        _args.append("--%s=%s" % (key, value))
+        _args = " ".join(self._dump_args)
+        if self._username:
+            _args += (" --username='%s'" % self._username)
+        if self._database:
+            _args += (" %s" % self._database)
 
-    if not fnmatch.filter(_args, "--authenticationDatabase=*"):
-        _args.append("--authenticationDatabase=admin")
-    
-    if _username:
-        _args.append(" -u '%s'" % _username)
-    if _password:
-        _args.append(" -p '%s'" % _password)
-    if _database:
-        _args.append(" -d %s" % _database)
+        if not fnmatch.filter(self._dump_env, "PGPASSWORD=*"):
+            self._dump_env.append("PGPASSWORD='%s'" % self._password)
 
-    _command = "mongodump %s" % " ".join(_args)
-    log.debug("Dump Command: %s" % _command)
-    e_code, gen = container.exec_run(cmd = _command, stream = True)
+        _command = "pg_dump %s" % _args
+        self._executeDump(_command)
+        
 
-    _file = "%s/%s_mongodb_%s.bak.gz" % (dumpdir, container.name, datetime.now().strftime("%Y%m%d%I%M%S"))
-    f = gzip.open(_file, "wb")
-    for item in gen:
-        f.write(item)
-    f.close()
+class MongoDumper(Dumper):
+    def dump(self):
+        """
+        Dump MongoDB
 
-def dumpMySQL(container):
-    """
-    Dump MySQL/MariaDB
-    """
-    _username = None
-    _password = None
-    _database = None
+        user, password and database have to be pass. This Using MongoDB, this values would not fetch by Environments.
+        Dump Parameter can be extend using dumper.mongo.<option>. For example "dumper.mongo.authenticationDatabase=admin"
+        will add "--authenticationDatabase=admin" as parameter to mongodump.
 
-    if "dumper.user" in container.labels:
-        _username = container.labels["dumper.user"]
-    elif "MYSQL_USER" in container.environments:
-        _username = container.environments["MYSQL_USER"]
+        Note that authenticationDatabase=admin will be set as default if it is not passed
+        """
 
-    if "dumper.password" in container.labels:
-        _password = container.labels["dumper.password"]
-    elif "MYSQL_PASSWORD" in container.environments:
-        _password = container.environments["MYSQL_PASSWORD"]
+        _args = ["--quiet", "--archive"]
+        _args.extend(self._dump_args)
 
-    if "dumper.database" in container.labels:
-        _database = container.labels["dumper.database"]
-    elif "MYSQL_DATABASE" in container.environments:
-        _database = container.environments["MYSQL_DATABASE"]
+        if not fnmatch.filter(_args, "--authenticationDatabase=*"):
+            _args.append("--authenticationDatabase=admin")
+        
+        if self._username:
+            _args.append(" -u '%s'" % self._username)
+        if self._password:
+            _args.append(" -p '%s'" % self._password)
+        if self._database:
+            _args.append(" -d %s" % self._database)
 
-    _args = ""
-    if _username:
-        _args += (" -u'%s'" % _username)
-    if _password:
-        _args += (" -p'%s'" % _password)
-    if _database:
-        _args += (" %s" % _database)
+        _command = "mongodump %s" % " ".join(_args)
+        self._executeDump(_command)
 
-    _command = "mysqldump %s" % _args
-    e_code, gen = container.exec_run(cmd = _command, stream = True)
+class FreeDumper(Dumper):
+    def dump(self):
+        """
+        Dump any Database
 
-    _file = "%s/%s_mysql_%s.sql.gz" % (dumpdir, container.name, datetime.now().strftime("%Y%m%d%I%M%S"))
-    f = gzip.open(_file, "wb")
-    for item in gen:
-        f.write(item)
-    f.close()
+        Use this Command to dump any database type. Just set dumper.command="..." to dump a database.
+        The command should dumop the Database to stdout. This Function will write it to a dump file.
+        """
+
+        if "dumper.command" in container.labels:
+            _command = "%s %s" % (container.labels["dumper.command"], " ".join(self._dump_args))
+        
+        self._executeDump(_command)
+
 
 def main():
     """
@@ -225,16 +262,20 @@ def main():
         _type = container.labels["dumper.type"] if "dumper.type" in container.labels else "mysql"
         log.info("Database Type: %s" % (_type))
 
+        dumper = None
         if _type in ["mysql", "maria", "mariadb"]:
-            dumpMySQL(container)
-        elif _type in ["mongo", "mongodb"]:
-            dumpMongoDB(container)
+            dumper = MySQLDumper(container)
         elif _type in ["postgres", "postgresql"]:
-            dumpPostgreSQL(container)
+             dumper = PostgresDumper(container)
+        elif _type in ["mongo", "mongodb"]:
+             dumper = MongoDumper(container)
         elif _type == "free":
-            dumpFree(container)
+            dumper = FreeDumper(container)
         else:
             print("Invalid dumper type")
+
+        dumper.dump()
+        dumper.cleanup()
 
 # Start
 main()
