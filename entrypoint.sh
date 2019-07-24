5a877a83d03f7bea54a5961e5000353efc570987
arogarth <arogarth@gmail.com>
2019-07-24 09:15:02 +0200

    Override crontab

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "${SCHEDULER}         /dumper.py" >> /var/spool/cron/crontabs/root
+echo "${SCHEDULER}         /dumper.py" > /var/spool/cron/crontabs/root
 
 cat /var/spool/cron/crontabs/root
 
